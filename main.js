ffdb007c284476659de5c330dd75c09a4ab2e256
LUCAS ALEXANDRE DA SILVA <25203702@dsn127m1242313.cfp127aluno.local>
2026-02-24 17:04:04 -0300

    feat: adiciona função de cadastro do código do produto e quantidade

--- main.js
+++ main.js
@@ -1,38 +1,79 @@
 'use strict'   
 
-function adicionarProduto(){
-    const produto = document.getElementById('produto')
+function adicionaCodigo(){
+    const codigo = document.getElementById('codigo-produto')
     const lista = document.getElementById('lista')
 
-    //lista.textContent = produto.value
+    const spanCodigo = document.createElement('span')
+    spanCodigo.textContent = codigo.value
 
-    const span = document.createElement('span')
-    span.textContent = produto.value
-
-    let nomeProduto = validarVazio(produto.value)
-
-    if(nomeProduto){
-        span.className = 'bg-blue-400 px-8 py-2 rounded-[5px]'
-        lista.appendChild(span)
+    let numeroCodigo = validarVazio(codigo.value)
+    
+    if(numeroCodigo){
+        spanCodigo.className = 'bg-blue-400 px-8 py-2 rounded-[5px]'
+        lista.appendChild(spanCodigo)
         limparCampo()
     }else{
         return false
     }
-
 }
 
-function validarVazio(produto){
-    let nomeProduto = String(produto)
+function adicionarProduto(){
+    const produto = document.getElementById('produto')
+    const lista = document.getElementById('lista')
 
-    if(nomeProduto == ''){
+    const spanProduto = document.createElement('span')
+    spanProduto.textContent = produto.value
+
+    let nomeProduto = validarVazio(produto.value)
+
+    if(nomeProduto){
+        spanProduto.className = 'bg-blue-400 px-8 py-2 rounded-[5px]'
+        lista.appendChild(spanProduto)
+        limparCampo()
+    }else{
+        return false
+    }
+}
+
+function adicionarQuantidadeProduto(){
+    const quantidade = document.getElementById('quantidade-produto')
+    const lista = document.getElementById('lista')
+
+    const spanQuantidade = document.getElementById('span')
+    spanQuantidade.textContent = quantidade.value
+
+    let numeroQuantidade = validarVazio(quantidade.value)
+
+    if(numeroQuantidade){
+        spanQuantidade.className = 'bg-blue-400 px-8 py-2 rounded-[5px]'
+        lista.appendChild(spanQuantidade)
+        limparCampo()
+    }else{
+        return false
+    }
+}
+
+function validarVazio(codigo, produto, quantidade){
+    let nomeProduto = String(produto)
+    let numeroCodigo = String(codigo)
+    let numeroQuantidade = Number(quantidade)
+
+    if(nomeProduto == '' || numeroCodigo == '' || numeroQuantidade == ''){
         return false
     }else{
-        return nomeProduto
+        return true
     }
 }
 
 function limparCampo(){
     document.getElementById('produto').value = ''
+    document.getElementById('codigo-produto').value = ''
+    document.getElementById('quantidade-produto').value = ''
 }
+
+
+
+
 
 
